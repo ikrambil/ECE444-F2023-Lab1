4bff065c0652cd42fe8855111c6d976a5ff46df5
Bilal Ikram <78551718+ikrambil@users.noreply.github.com>
2023-09-20 22:52:51 -0400

    Finish Activity Part 4 Step 2

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'lnlk;jdvsnd;jkvbsd'
@@ -17,6 +17,7 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 
@@ -38,6 +39,12 @@ def index():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        
+        old_email = session.get('email')
+        if old_email is not None and old_email != form.name.data:
+            flash('Looks like you have changed your email!')
+        
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
